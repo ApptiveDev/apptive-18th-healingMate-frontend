f93c31ac7c7e1b2ed331c1fb4bb505a47503e03f
문정윤 <halfmoonjy99@pusan.ac.kr>
2023-07-02 21:45:50 +0900

    [feat] 중간 정리

---
+++ src/pages/community/board/1.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const BoardMain = () => {
+  return (
+    <div>
+      <div className="CommuTitle">
+        만성콩팥병
+        <br />
+        (Chronic renal failure)
+      </div>
+    </div>
+  );
+};
+
+export default BoardMain;
