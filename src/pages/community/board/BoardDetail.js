4eb1f328c544f75e372e5e74c6d409df92e01bf7
taeyoung Kim <100909703+taeyomi@users.noreply.github.com>
2023-07-22 17:54:42 +0900

    Revert "Feature/community mainpage"

--- src/pages/community/board/BoardDetail.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const BoardDetail = () => {
-  return <div></div>;
-};
-
-export default BoardDetail;
--- src/pages/main/Main.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Main.scss";
-
-const main = () => {
-  return <div className="main"></div>;
-};
-
-export default main;
